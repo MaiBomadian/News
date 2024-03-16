8e297ce069b7e77b4d96e656f409107e13cdb4f3
Mai <MaiBomadian94>
2024-03-17 00:36:35 +0200

    * Handling image of article

--- lib/core/widgets/article_item_widget.dart
+++ lib/core/widgets/article_item_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/core/config/constants.dart';
 import 'package:news_app/models/article_data_model.dart';
@@ -14,9 +14,35 @@ class ArticleItemWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(articles.urlToImage ?? '')),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: CachedNetworkImage(
+              imageUrl: articles.urlToImage ?? '',
+              imageBuilder: (context, imageProvider) => Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(
+                Icons.error,
+                size: 40,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          // ClipRRect(
+          //     borderRadius: BorderRadius.circular(10),
+          //     child: Image.network(articles.urlToImage ?? '')),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10),
             child: Text(
--- lib/core/widgets/articles_list_widget.dart
+++ lib/core/widgets/articles_list_widget.dart
@@ -35,3 +35,17 @@ class ArticlesListWidget extends StatelessWidget {
     );
   }
 }
+// CachedNetworkImage(
+// imageUrl: "http://via.placeholder.com/200x150",
+// imageBuilder: (context, imageProvider) => Container(
+// decoration: BoxDecoration(
+// image: DecorationImage(
+// image: imageProvider,
+// fit: BoxFit.cover,
+// colorFilter:
+// ColorFilter.mode(Colors.red, BlendMode.colorBurn)),
+// ),
+// ),
+// placeholder: (context, url) => CircularProgressIndicator(),
+// errorWidget: (context, url, error) => Icon(Icons.error),
+// ),
